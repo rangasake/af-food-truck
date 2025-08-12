0bb06803571eb754b5c134c5c2eb9ac4afe4eb8b
ranga <76901674+rangasake@users.noreply.github.com>
2025-08-12 11:18:00 +0530

    Update include-header.js

--- foodTruck/js/include-header.js
+++ foodTruck/js/include-header.js
@@ -1,4 +1,4 @@
-fetch('/header.html')
+fetch('af-food-truck/foodTruck//header.html')
   .then(response => {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
