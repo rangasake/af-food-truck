b237b21381e1c5e4146ec0a61cfddc91e0467040
ranga <76901674+rangasake@users.noreply.github.com>
2025-08-12 11:05:47 +0530

    Initial upload
    Initial upload

---
+++ foodTruck/js/include-header.js
@@ -0,0 +1,13 @@
+fetch('/header.html')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.text();
+  })
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Could not load header:', error);
+  });
